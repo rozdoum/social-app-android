7442b36c8e99f6254ad2b8face4c4d2590103729
Kristina Skachkova <kristina.skachkova@rozdoum.com>
2016-10-28 14:18:31 +0300

    IA-1060: initialised firebase

---
+++ app/src/main/java/com/rozdoum/socialcomponents/DatabaseHelper.java
@@ -0,0 +1,10 @@
+package com.rozdoum.socialcomponents;
+
+/**
+ * Created by Kristina on 10/28/16.
+ */
+
+public class DatabaseHelper {
+
+    public static final String TAG = DatabaseHelper.class.getSimpleName();
+}
