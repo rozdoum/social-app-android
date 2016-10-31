17b34860445734f3e78ba3072fc9bd63aabe7987
Kristina Skachkova <kristina.skachkova@rozdoum.com>
2016-10-31 20:25:00 +0200

    IA-1061: merged changes

--- app/src/main/java/com/rozdoum/socialcomponents/activities/MainActivity.java
+++ app/src/main/java/com/rozdoum/socialcomponents/activities/MainActivity.java
@@ -5,9 +5,20 @@ import android.support.design.widget.FloatingActionButton;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.ListView;
+
 import com.rozdoum.socialcomponents.R;
+import com.rozdoum.socialcomponents.adapters.PostsAdapter;
+import com.rozdoum.socialcomponents.managers.PostManager;
+import com.rozdoum.socialcomponents.managers.listeners.OnDataChangedListener;
+import com.rozdoum.socialcomponents.model.Post;
+
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
+
+    private ListView postsListView;
+    private PostsAdapter postsAdapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -24,6 +35,19 @@ public class MainActivity extends AppCompatActivity {
                 }
             });
         }
+
+        postsListView = (ListView)findViewById(R.id.postsListView);
+        postsAdapter = new PostsAdapter();
+        postsListView.setAdapter(postsAdapter);
+
+        OnDataChangedListener<Post> onPostsDataChangedListener = new OnDataChangedListener<Post>() {
+            @Override
+            public void onListChanged(List<Post> list) {
+                postsAdapter.setList(list);
+            }
+        };
+
+        PostManager.getInstance(getApplicationContext()).getPosts(onPostsDataChangedListener);
     }
 
     private void openCreatePostActivity() {
