a6ad6edd93fa1648704a1e3173527c8b9ffca0a1
Alexey Batishev <alexey.batishev@rozdoum.com>
2016-12-27 14:22:08 +0200

    SCA-76: removed empty spaces in posts description in posts list

--- app/src/main/java/com/rozdoum/socialcomponents/Constants.java
+++ app/src/main/java/com/rozdoum/socialcomponents/Constants.java
@@ -10,4 +10,8 @@ public class Constants {
         public static final int MAX_AVATAR_SIZE = 1280; //px, side of square
         public static final int MIN_AVATAR_SIZE = 100; //px, side of square
     }
+
+    public static class Post {
+        public static final int MAX_DESCRIPTION_LENGTH_IN_LIST = 300; //characters
+    }
 }
